1a3dd5d06bbc9153cc3d7a080a6a47ae5db9264d
ShavelD <legrand.97@mail.ru>
2023-03-05 23:13:46 +0300

    fix

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -2,8 +2,9 @@ import React from 'react';
 import s from './Projects.module.scss'
 import MyProject from "./myProjects/MyProject";
 import Title from "./../common/components/title/Title"
-import TodoImage from "./../assets/image/images.png"
-import SocialImage from "./../assets/image/SocialNetworking.png"
+import TodoImage from "./../assets/image/todolist2.jpg"
+import SocialImage from "./../assets/image/blog1.jpg"
+import FlashImage from "./../assets/image/flash4.webp"
 import sContainer from "../common/styles/Container.module.scss";
 import Fade from 'react-reveal/Fade';
 
@@ -18,41 +19,37 @@ const Projects = () => {
         backgroundImage: `url(${TodoImage})`,
     }
 
+    const flashCards = {
+        backgroundImage: `url(${FlashImage})`,
+    }
+
     return (
         <div id='Projects' className={`${s.projectsBlock} ${sContainer.padding}`}>
             <div className={`${s.projectsContainer} ${sContainer.container}`}>
-                <Fade right>
+                {/*<Fade right>*/}
                     <Fade top>
                     <Title title={"My Projects"}/>
                     </Fade>
                 <div className={s.projects}>
-                    <MyProject style={social}
-                               titleProject={'Social Network'}
-                               description={"Lorem ipsum dolor ipsum dolor sit " +
-                                   "dolor  sit dolor sit dolor sit dolor sit"}/>
+                    <Fade top>
                     <MyProject style={todolist}
                                titleProject={'Todolist'}
+                               link={"https://shaveld.github.io/todolist-sh-ts/#/"}
                                description={"Lorem sit dolor sit dolor " + "ipsumt"}/>
-                    <MyProject style={todolist}
-                               titleProject={'1'}
+                    </Fade>
+                    <Fade bottom>
+                    <MyProject style={flashCards}
+                               titleProject={'Learning cards'}
                                description={"Lorem sit dolor sit dolor " + "ipsumt"}/>
-                    <MyProject style={todolist}
-                               titleProject={'2'}
-                               description={"Lorem sit dolor sit dolor " + "ipsumt"}/>
-                    <MyProject style={todolist}
-                               titleProject={'3'}
-                               description={"Lorem sit dolor sit dolor " + "ipsumt"}/>
-                    <MyProject style={todolist}
-                               titleProject={'4'}
-                               description={"Lorem sit dolor sit dolor " + "ipsumt"}/>
-                    <MyProject style={todolist}
-                               titleProject={'5'}
-                               description={"Lorem sit dolor sit dolor " + "ipsumt"}/>
-                    <MyProject style={todolist}
-                               titleProject={'6'}
-                               description={"Lorem sit dolor sit dolor " + "ipsumt"}/>
+                    </Fade>
+                    <Fade top>
+                        <MyProject style={social}
+                                   titleProject={'Social Network'}
+                                   description={"Lorem ipsum dolor ipsum dolor sit " +
+                                       "dolor  sit dolor sit dolor sit dolor sit"}/>
+                    </Fade>
                 </div>
-             </Fade>
+             {/*</Fade>*/}
         </div>
   </div>
 )
--- src/projects/myProjects/MyProject.js
+++ src/projects/myProjects/MyProject.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import s from './MyProject.module.scss'
-//
-// const MyProject = (props) => {
-//
-//     return (
-//         <div className={s.project}>
-//             <div className={s.imgContainer} style={props.style}>
-//                 <a className={s.viewBtn}>Смотреть</a>
-//             </div>
-//             <div className={s.myProject}>
-//                <h3 className={s.titleProject}>{props.titleProject}</h3>
-//                <span className={s.description}>{props.description}</span>
-//             </div>
-//           </div>
-//     );
-// };
-//
-// export default MyProject;
+
+const MyProject = (props) => {
+
+    return (
+        <div className={s.project}>
+            <div className={s.imgContainer} style={props.style}>
+                <a href={props.link} className={s.links}>Смотреть</a>
+            </div>
+            <div className={s.myProject}>
+               <h3 className={s.titleProject}>{props.titleProject}</h3>
+               <span className={s.description}>{props.description}</span>
+            </div>
+          </div>
+    );
+};
+
+export default MyProject;
 //
 // import React from 'react';
 // import s from './Project.module.scss'
@@ -29,25 +29,25 @@ import s from './MyProject.module.scss'
 //     linkDemo: string,
 //     linkCode: string
 // }
-
-const MyProject = (props) => {
-    return (
-        <div className={s.project}>
-            <div className={s.img} style={props.style}>
-                <div className={s.overlay}>
-                    <div className={s.description}>
-                        <h3>{props.title}</h3>
-                        <p>{props.description}</p>
-                        <div className={s.links}>
-                            <a href={props.linkDemo} className={s.btnDemo} target="_blank" rel="noreferrer">view
-                                demo</a>
-                            <a href={props.linkCode} className={s.btnCode} target="_blank" rel="noreferrer">view
-                                code</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
-export default MyProject;
+//
+// const MyProject = (props) => {
+//     return (
+//         <div className={s.project}>
+//             <div className={s.img} style={props.style}>
+//                 <div className={s.overlay}>
+//                     <div className={s.description}>
+//                         <h3>{props.title}</h3>
+//                         <p>{props.description}</p>
+//                         <div className={s.links}>
+//                             <a href={props.linkDemo} className={s.btnDemo} target="_blank" rel="noreferrer">view
+//                                 demo</a>
+//                             <a href={props.linkCode} className={s.btnCode} target="_blank" rel="noreferrer">view
+//                                 code</a>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+// export default MyProject;
